e4463325fec6ffb33dda89e5ace67c4aafde5290
alanwhiterockai <alan@ml.cafe>
2023-01-27 14:33:30 -0500

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,114 @@
+TOOL_QUALITY_LIST = [
+    ("Apprentice+", None),
+    ("Journeyman+", None),
+    ("Craftsman+", None),
+    ("Artisan+", None),
+    ("Adept+", None),
+    ("Expert+", None),
+    ("Master+", None),
+    ("Legendary+", None),
+]
+
+FOLLOWER_LIST = [
+    ("Any Animal", None),
+    ("Bumbabloopfnoop", None),
+    ("Cat", None),
+    ("Dog", None),
+    ("Fillifryth", None),
+    ("NPC", None),
+    ("Pig", None),
+    ("Sheep", None),
+    ("Spouse", None),
+    ("Has Greens", None),
+]
+
+GATHER_TYPE_LIST = [("Growing", None), ("Fishing", None), ("Mining", None), ("Shooting", None), ("Gathering", None)]
+
+WEATHER_LIST = [
+    ("Blizzard", None),
+    ("Foggy", None),
+    ("Hot", None),
+    ("Mild", None),
+    ("Overcast", None),
+    ("Sunny", None),
+    ("Windy", None),
+    ("Snow", None),
+    ("Not Rain", None),
+    ("Rain", None),
+    ("Storm", None),
+    ("Pollen", None),
+    ("Bubbles", None),
+    ("Meteors", None),
+    ("Bloodmoon", None),
+    ("Eclipse", None),
+    ("Under Moonbeams", None),
+    ("Strange", None)
+]
+
+TRAIT_LIST = [
+    ("Blessed", None),
+    ("Bravery", None),
+    ("Cowardly", None),
+    ("Cursed", None),
+    ("Illusion", None),
+    ("Negative", None),
+    ("Scent", None),
+    ("Stench", None),
+    ("Agility", None),
+    ("Constitution", None),
+    ("Intelligence", None),
+    ("Strength", None),
+    ("Toughness", None),
+    ("Clumsy", None),
+    ("Flatulent", None),
+    ("Goddess Jinx", None),
+]
+
+SEASON_LIST = [
+    ("Autumn", None),
+    ("Autumn w1", None),
+    ("Autumn w2", None),
+    ("Spring", None),
+    ("Spring w1", None),
+    ("Spring w2", None),
+    ("Summer", None),
+    ("Summer w1", None),
+    ("Summer w2", None),
+    ("Winter", None),
+    ("Winter w1", None),
+    ("Winter w2", None),
+]
+
+TIME_LIST = [
+    ("Moonsday (1)", None),
+    ("Truthsday (2)", None),
+    ("Woesday (3)", None),
+    ("Turnsday (4)", None),
+    ("Freylsday (5)", None),
+    ("Satyrsday (6)", None),
+    ("Solsday (7)", None),
+    ("Morning", None),
+    ("Afternoon", None),
+    ("Evening", None),
+    ("Night", None),
+    ("Dawn", None),
+    ("Dusk", None),
+    ("Dawn/Dusk", None),
+    ("Midnight", None),
+    ("Even days", None),
+    ("Odd days", None),
+    ("Vale Market days", None),
+    ("Exact Hour", None),
+]
+
+POO_LIST = ["Sheep", "Pig"]
+
+OPTION_MENU = {
+    "gatherable_type": "Gather Type",
+    "tool": "Tool Proficiency",
+    "follower": "Follower",
+    "weather": "Weather",
+    "trait": "Trait",
+    "season": "Season",
+    "time": "Time",
+}
